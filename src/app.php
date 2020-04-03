aaf799f945103799a81bb8c6f25498a6aa433334
Hari KT <kthari85@gmail.com>
2020-04-03 20:11:13 +0530

    Skelton

--- src/app.php
+++ src/app.php
@@ -1,10 +1,10 @@
 <?php
 
-use Silex\Application;
-use Silex\Provider\AssetServiceProvider;
-use Silex\Provider\TwigServiceProvider;
-use Silex\Provider\ServiceControllerServiceProvider;
-use Silex\Provider\HttpFragmentServiceProvider;
+use Mascot\Application;
+use Mascot\Provider\AssetServiceProvider;
+use Mascot\Provider\TwigServiceProvider;
+use Mascot\Provider\ServiceControllerServiceProvider;
+use Mascot\Provider\HttpFragmentServiceProvider;
 
 $app = new Application();
 $app->register(new ServiceControllerServiceProvider());
--- src/console.php
+++ src/console.php
@@ -3,10 +3,9 @@
 use Symfony\Component\Console\Application;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputOption;
 
-$console = new Application('My Silex Application', 'n/a');
+$console = new Application('My Mascot Application', 'n/a');
 $console->getDefinition()->addOption(new InputOption('--env', '-e', InputOption::VALUE_REQUIRED, 'The Environment name.', 'dev'));
 $console->setDispatcher($app['dispatcher']);
 $console
